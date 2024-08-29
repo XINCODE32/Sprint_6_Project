5919dcd696ff16d24467df921e39cfd8b45dcc30
XINCODE32 <123253404+XINCODE32@users.noreply.github.com>
2024-08-29 22:36:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ st.header('Compare price distribution between condition and type')
 fig_1 = px.histogram(df, x= 'condition', y= 'price', color= type)
 st.write(fig_1)
 
-filter_year = df[df['model_year'] > 1980]
-filter_odometer = df[df['odometer'] > 400000]
+filter_year = df[df['model_year'] > 1980]['model_year']
+filter_odometer = df[df['odometer'] > 400000]['odometer']
 st.header('Compare price distribution between model year and odometer')
-fig_2 = px.scatter(df, x=filter_year, y= filter_odometer, color= 'condition', marginal_x= "box", marginal_y= "violin")
+fig_2 = px.scatter(df, x=filter_year, y= ilter_odometer, color= 'condition', marginal_x= "box", marginal_y= "violin")
 st.write(fig_2)
